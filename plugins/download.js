76e3607e03af0aa30e29be8e2bc57a060d0192ab
RAHUL-MASTER <rahulhiran37@gmail.com>
2025-01-13 10:39:15 +0530

    Add files via upload

---
+++ plugins/download.js
@@ -0,0 +1,284 @@
+const {
+    Sparky,
+    isPublic
+} = require("../lib/plugins.js");
+let gis = require("g-i-s");
+const axios = require('axios');
+const fetch = require('node-fetch');
+const {
+    API
+} = require("../config.js");
+Sparky(
+    {
+        name: "insta",
+        fromMe: isPublic,
+        desc: "Instagram downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+        if (!args) return await m.reply("_Enter Link_");
+        let dl = await client.sendMessage(m.jid, {
+            text: "_Downloading..._"
+        }, {
+            quoted: m
+        })
+        try {
+            const res = await axios.get(`${API}/api/downloader/igdl?url=${args}`)
+            let response = await res.data
+            for (let i of response.data) {
+                await m.sendMsg(m.jid, i.url, { quoted: m }, i.type)
+            }
+        } catch (e) {
+            client.sendMessage(m.jid, {
+                text: `_Error_`, edit: dl.key
+            })
+        }
+    }
+);
+
+Sparky(
+    {
+        name: "story",
+        fromMe: isPublic,
+        desc: "Instagram story downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+        if (!args) return await m.reply("_Enter Link_");
+        let dl = await client.sendMessage(m.jid, {
+            text: "_Just a moment..._"
+        }, {
+            quoted: m
+        })
+
+        let url = args
+        let res = await axios.get(`${API}/api/downloader/story?url=${url}`)
+        let response = await res.data
+        let data = response.data[0]
+        let datai = `Total Stories\nUrl : ${url}\n\n`
+        for (let i = 1; i < response.data.length + 1; i++) {
+            datai += `_${i} . ${i}/${response.data.length} - ${response.data[i - 1].type}_\n`
+        }
+        datai += '\nReply with Number'
+        m.sendMsg(m.jid, datai, {
+            edit: dl.key
+        })
+    }
+);
+
+////////////
+Sparky(
+    {
+        name: "yt",
+        fromMe: isPublic,
+        desc: "Select video quality to get the download link",
+        category: "downloader",
+    },
+    async ({ m, client, args }) => {
+        try {
+            args = args || m.quoted?.text;
+            if (!args) return await m.reply("_Enter Link Or Reply To a Link_");
+            let url = args
+            m.reply("_Please hold on This May Take Sometime._");
+            let sample = await fetch(`https://viper.devstackx.in/api/v1/ytdl?query=${url}`);
+            let data = await sample.json();
+            let mediaList = data.data.media;
+
+            let dl = await client.sendMessage(m.jid, { text: 'Processing your download options...' }, { quoted: m });
+
+            let datai = `_*YouTube Downloader*_\nUrl : ${url}\n\n`
+            for (let i = 1; i < mediaList.length + 1; i++) {
+                datai += `_${i} . ${mediaList[i - 1].type} - ${mediaList[i - 1].quality}_\n`
+            }
+            datai += '\nReply with Number'
+            m.sendMsg(m.jid, datai, {
+                edit: dl.key
+            })
+        } catch (e) {
+            await m.reply(`Error: ${e.message}`);
+        }
+    }
+);
+
+/////////////
+
+
+Sparky(
+    {
+        name: "img",
+        fromMe: isPublic,
+        desc: "Google Image search",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        try {
+            async function gimage(query, amount = 5) {
+                let list = [];
+                return new Promise((resolve, reject) => {
+                    gis(query, async (error, result) => {
+                        for (
+                            var i = 0;
+                            i < (result.length < amount ? result.length : amount);
+                            i++
+                        ) {
+                            list.push(result[i].url);
+                            resolve(list);
+                        }
+                    });
+                });
+            }
+            if (!args) return await m.reply("_Enter Query,Number_");
+            let [query,
+                amount] = args.split(",");
+            let result = await gimage(query, amount);
+            await m.reply(
+                `_Downloading ${amount || 5} images for ${query}_`
+            );
+            for (let i of result) {
+                await m.sendMsg(m.jid, i, {}, "image")
+            }
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+);
+
+
+Sparky(
+    {
+        name: "gdrive",
+        fromMe: isPublic,
+        desc: "Instagram story downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+        if (!args) return await m.reply("_Enter Link_");
+        var document = await fetch(`${API}/api/downloader/gdrive?url=${args}`);
+        var zip = await document.json();
+        client.sendMessage(m.jid, { document: { url: zip.data.downloadUrl }, fileName: `${zip.data.fileName}`, mimetype: "application/x-zip-compressed" }, { quoted: m })
+    }
+);
+
+Sparky(
+    {
+        name: "mediafire",
+        fromMe: isPublic,
+        desc: "Instagram story downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+        if (!args) return await m.reply("_Enter Link_");
+        var document = await fetch(`${API}/api/downloader/mediafire?url=${args}`);
+        var zip = await document.json();
+        client.sendMessage(m.jid, { document: { url: zip.data.link }, fileName: `${zip.data.name}`, mimetype: "application/zip" }, { quoted: m })
+    }
+);
+
+
+Sparky(
+    {
+        name: "xvdl",
+        fromMe: isPublic,
+        desc: "Instagram story downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+        if (!args) return await m.reply("_Enter Link/Reply to a link_");
+        m.reply("_Downloading..._")
+        let xvdl = await fetch(`${API}/api/downloader/xdl?url=${args}`);
+        var data = await xvdl.json();
+
+        client.sendMessage(m.jid, { video: { url: data.data }, caption: "ðŸ¤¤ðŸ’¦" }, { quoted: m })
+    }
+);
+
+
+Sparky({
+    name: "play",
+    fromMe: isPublic,
+    desc: "Download and play your favorite audio or video directly from a link.",
+    category: "downloader",
+},
+async ({
+    m,
+    client,
+    args
+}) => {
+    try{
+        args = args || m.quoted?.text;
+if (!args) return await m.reply("_enter query or reply to link_");
+let mes = await client.sendMessage(m.jid, { text : `Please wait, searching...` } , { quoted : m })
+let sample = await fetch(`https://viper.devstackx.in/api/v1/yta?query=${args}`);
+var data = await sample.json();
+const songbuff = await (await fetch(`${data.data.downloadUrl}`)).buffer()
+client.sendMessage(m.jid, { text : `_Downloading : ${data.data.title}_` , edit : mes.key })
+await client.sendMessage(m.jid , {audio : songbuff,  mimetype : 'audio/mpeg'} , { quoted : m })
+    } catch(e) {
+        m.reply(e)
+    }
+    }
+);
+
+Sparky({
+    name: "song",
+    fromMe: isPublic,
+    desc: "Download and play your favorite audio or video directly from a link.",
+    category: "downloader",
+},
+async ({
+    m,
+    client,
+    args
+}) => {
+    try{
+        args = args || m.quoted?.text;
+if (!args) return await m.reply("_enter query or reply to link_");
+let mes = await client.sendMessage(m.jid, { text : `Please wait, searching...` } , { quoted : m })
+let sample = await fetch(`https://viper.devstackx.in/api/v1/yta?query=${args}`);
+var data = await sample.json();
+const songbuff = await (await fetch(`${data.data.downloadUrl}`)).buffer()
+client.sendMessage(m.jid, { text : `_Downloading : ${data.data.title}_` , edit : mes.key })
+await client.sendMessage(m.jid , {audio : songbuff,  mimetype : 'audio/mpeg'} , { quoted : m })
+    } catch(e) {
+        m.reply(e)
+    }
+    }
+);
+
+// Sparky(
+//     {
+//         name: "ytv",
+//         fromMe: isPublic,
+//         category: "downloader",
+//         desc: "",
+//     },
+//     async ({
+//         m, client, args
+//     }) => {
+//         args = args || m.quoted?.text;
+//         if (!args) return await m.reply("_Reply to a link_");
+//         let ytdlmsg = await client.sendMessage(m.jid, { text: "_Please Wait...._" }, { quoted: m })
+//         let data = await fetch(`${API}/api/downloader/ytv?url=${args}`);
+//         var result = await data.json();
+//         await client.sendMessage(m.jid, { text: result.data.title, edit: ytdlmsg.key })
+//         client.sendMessage(m.jid, { video: { url: result.data.dlink }, caption: result.data.title }, { quoted: m })
+//     }
+// );
---
+++ plugins/group.js
@@ -0,0 +1,185 @@
+const {
+    Sparky,
+    
+    isPublic
+} = require("../lib/plugins.js");
+const font = require("@viper-x/fancytext");
+
+
+Sparky(
+    {
+        name: "tag",
+        fromMe: true,
+        desc: "Tags content by mentioning all participants",
+        category: "group"
+    },
+    async ({
+        m, client, args
+    }) => {
+        const arg = m.quoted || args;
+        if (!arg) return m.reply("_Enter or reply to a message_");
+        const groupMetadata = await client.groupMetadata(m.jid);
+        const jids = groupMetadata.participants.map((participant) => participant.id);
+        let messageContent;
+        if (typeof arg === "string") {
+            messageContent = {
+                text: arg,
+                mentions: jids
+            };
+        } else {
+            messageContent = arg;
+            const options = { contextInfo: { mentionedJid: jids } };
+            return await m.forwardMessage(m.jid, messageContent, options);
+        }
+        await client.sendMessage(m.jid, messageContent, { quoted: m });
+    }
+);
+
+
+Sparky(
+    {
+        name: "tagall",
+        fromMe: true,
+        desc: "Tags all participants with a numbered list",
+        category: "sudo",
+    },
+    async ({ client, m, args }) => {
+        try {
+            const groupMetadata = await client.groupMetadata(m.jid).catch(e => null);
+            if (!groupMetadata) return m.reply("Failed to fetch group metadata.");
+            const participants = groupMetadata.participants;
+            let msg = "";
+            participants.forEach((participant, index) => {
+                msg += `${index + 1}. @${participant.id.split('@')[0]}\n`;
+            });
+            const jids = participants.map((participant) => participant.id);
+            await client.sendMessage(m.jid, { 
+                text: msg, 
+                mentions: jids 
+            }, { quoted: m });
+            
+        } catch (e) {
+            console.error(e);
+            m.reply("An error occurred while processing the command.");
+        }
+    }
+);
+
+Sparky(
+    {
+        name: "invite",
+        fromMe: isPublic,
+        desc: "Share's Group invitation link",
+        category: "group"
+    },
+    async ({
+        m, client, args
+    }) => {
+
+        if (!await m.isAdmin(client.user.id)) return m.reply("Admin access not conferred.")
+        if (!await m.isAdmin(m.sender)) return m.reply("Only for authorized administrators.")
+        let code = await client.groupInviteCode(m.jid)
+        return m.reply('https://chat.whatsapp.com/' + code)
+    })
+
+Sparky(
+    {
+        name: "mute",
+        fromMe: isPublic,
+        desc: "Mutes the group.",
+        category: "group"
+    },
+    async ({
+        m, client, args
+    }) => {
+
+        if (!await m.isAdmin(client.user.id)) return m.reply("Admin access not conferred.")
+        if (!await m.isAdmin(m.sender)) return m.reply("Only for authorized administrators.")
+        await client.groupSettingUpdate(m.jid, 'announcement');
+        return await m.reply("Messages restricted to group admins.");
+    })
+
+Sparky(
+    {
+        name: "unmute",
+        fromMe: isPublic,
+        desc: "Unmutes the group",
+        category: "group"
+    },
+    async ({
+        m, client, args
+    }) => {
+
+        if (!await m.isAdmin(client.user.id)) return m.reply("Admin access not conferred.")
+        if (!await m.isAdmin(m.sender)) return m.reply("Only for authorized administrators.")
+        await client.groupSettingUpdate(m.jid, 'not_announcement');
+        return await m.reply("Messages Unrestricted.");
+    })
+
+Sparky(
+    {
+        name: "promote",
+        fromMe: isPublic,
+        desc: "Promotes a user to admin",
+        category: "group"
+    },
+    async ({
+        m, client, args
+    }) => {
+        try {
+            if (!await m.isAdmin(client.user.id)) return m.reply("Admin access not conferred.")
+            if (!await m.isAdmin(m.sender)) return m.reply("Only for authorized administrators.")
+
+            if (!(args || m.quoted)) return m.reply("Mention a user.")
+            if (args) {
+                var user = args.replace("@", "") + '@s.whatsapp.net';
+            } else if (m.quoted.sender) {
+                var user = m.quoted.sender;
+            } else if (m.mentionedJid) {
+                var user = args + '@s.whatsapp.net';
+            }
+
+            if (await m.isAdmin(user) === true) return m.reply("The user is currently in an admin role.");
+
+            await client.groupParticipantsUpdate(m.jid, [user], "promote");
+            m.sendMsg(m.jid , `@${user.split("@")[0]} promoted to admin role.`, { mentions : [user] , quoted : m})
+        } catch (e) {
+            console.log(e);
+        }
+
+    })
+
+Sparky(
+    {
+        name: "demote",
+        fromMe: isPublic,
+        desc: "Demotes a user from admin",
+        category: "group"
+    },
+    async ({
+        m, client, args
+    }) => {
+        try {
+
+            if (!await m.isAdmin(client.user.id)) return m.reply("Admin access not conferred.")
+            if (!await m.isAdmin(m.sender)) return m.reply("Only for authorized administrators.")
+            if (!(args || m.quoted)) return m.reply("Mention a user.")
+            if (args) {
+                var user = args.replace("@", "") + '@s.whatsapp.net';
+            } else if (m.quoted.sender) {
+                var user = m.quoted.sender;
+            } else if (m.mentionedJid) {
+                var user = args + '@s.whatsapp.net';
+            }
+
+            if (!await m.isAdmin(user)) return m.reply("Admin access not extended to the user.");
+
+            await client.groupParticipantsUpdate(m.jid, [user], "demote");
+            m.sendMsg(m.jid , `@${user.split("@")[0]} demoted from admin role!`, { mentions : [user] , quoted : m })
+
+        } catch (e) {
+            console.log(e);
+        }
+
+    })
+    
